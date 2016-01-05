16e03839abefdf9e41135c5ac4d744365d008e22
bellmounte <ryan.dacquisto@gmail.com>
2016-01-05 09:46:22 -0800

    Cleaning up extension after sale.

--- agecheck.js
+++ agecheck.js
@@ -1,9 +1,9 @@
-(function () {
+(function (d) {
 	'use strict';
 
 	setTimeout(function () {
 		console.log('Passing Age Check');
-		document.getElementById('ageYear').value ='1910';
-		document.querySelectorAll('a.btnv6_blue_hoverfade.btn_small')[0].click();
+		d.getElementById('ageYear').value = '1910';
+		d.querySelector('a.btnv6_blue_hoverfade.btn_small').click();
 	}, 500);
-})();
+})(document);
