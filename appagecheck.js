203dfa5f389ccf1c6cd6621cbccba3fd80d508b2
Ryan D'Acquisto <rdacquisto@zulily.com>
2017-03-06 07:34:26 -0800

    Add alternative age check script.

---
+++ appagecheck.js
@@ -0,0 +1,8 @@
+(function () {
+	'use strict';
+
+	setTimeout(function () {
+		console.log('Passing Content Check');
+		document.querySelectorAll('a.btn_grey_white_innerfade.btn_medium')[0].click();
+	}, 500);
+})();
